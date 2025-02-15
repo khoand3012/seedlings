e71c49c456c4e182c036b5eb523e4a3916fe9f0b
Khoa Nguyen <aaronfebruary95@gmail.com>
2025-02-16 00:43:00 +0700

    Add initial pages and github authorization

---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+
+export default async function Navbar() {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="site-logo"
+            height={30}
+            width={144}
+          ></Image>
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href={"/startup/create"}>
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
+              </form>
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
